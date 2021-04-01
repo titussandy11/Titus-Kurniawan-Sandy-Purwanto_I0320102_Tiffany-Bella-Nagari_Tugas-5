c2a6ef88f3f47338cc913978cf23e194e9e92f56
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-04-02 00:46:10 +0700

    Exercise 5.3

--- I0320102_exercise5.3.py
+++ I0320102_exercise5.3.py
@@ -1,1 +1,23 @@
+#Titus Kurniawan Sandy Purwanto
+#I0320102
+#Kelas C
+#Exercise 5.3
 
+#penggunaan if untuk tiga kasus atau lebih
+#input bilangan
+print('Masukkan koordinat')
+x = int(input('Masukkan nilai x : '))
+y = int(input('Masukkan nilai y : '))
+info = 'Koordinat (' + str(x) + ',' + str(y) + ') berada pada kuadran '
+#memeriksa nilai x dan y
+if x>0 and y>0 :
+    print(info + 'I')
+elif x<0 and y>0 :
+    print(info + 'II')
+elif x<0 and y<0 :
+    print(info + 'III')
+elif x>0 and y<0 :
+    print(info + 'IV')
+else :
+    pass
+print()
