ed273ecf76fd48ca347e6ef4970f6805babeb810
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-04-02 00:34:15 +0700

    Exercise 5.1

--- I0320102_exercise5.1.py
+++ I0320102_exercise5.1.py
@@ -1,1 +1,12 @@
+#Titus Kurniawan Sandy Purwanto
+#I0320102
+#Kelas C
+#Exercise 5.1
 
+#penggunaan if untuk satu kasus
+#inputkan bilangan
+bilangan = int(input("Masukkan bilangan bulat : "))
+#memeriksa bilangan
+if bilangan %2 == 0 :
+    print("%d adalah bilangan genap" % bilangan)
+print()
