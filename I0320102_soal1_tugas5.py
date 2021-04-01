293bf919e91a41c22d5d717d03331860966debe3
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-04-02 01:00:56 +0700

    Soal 1

--- I0320102_soal1_tugas5.py
+++ I0320102_soal1_tugas5.py
@@ -1,1 +1,14 @@
+#Titus Kurniawan Sandy Purwanto
+#I0320102
+#Kelas C
+#Soal 1
 
+print('-----Virtual Front Desk Reception-----')
+nama = str(input('Silahkan masukkan nama anda : '))
+jenis_kelamin = str(input('Silahkan masukkan jenis kelamin anda (L/P) : '))
+if jenis_kelamin == 'L' or jenis_kelamin == 'l' :
+    print('Selamat datang, Bapak', nama, '!')
+elif jenis_kelamin == 'P' or jenis_kelamin == 'p' :
+    print('Selamat datang, Ibu', nama, '!')
+else :
+    print('Mohon masukkan nama dan jenis kelamin anda dengan benar')
