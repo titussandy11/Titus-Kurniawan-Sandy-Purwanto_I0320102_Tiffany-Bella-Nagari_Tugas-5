ac1669f1decb233f638f0d191e088442ff333e31
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-04-02 00:37:49 +0700

    Exercise 5.2

--- I0320102_exercise5.2.py
+++ I0320102_exercise5.2.py
@@ -1,1 +1,13 @@
+#Titus Kurniawan Sandy Purwanto
+#I0320102
+#Kelas C
+#Exercise 5.2
 
+#penggunaan if untuk dua kasus
+#inputkan bilangan
+bilangan = int(input('MAsukkan bilangan bulat : '))
+#memeriksa bilangan
+if bilangan %2 == 0 :
+    print("%d adalah bilangan genap" % bilangan)
+else :
+    print("%d adalah bilangan ganjil" % bilangan)
